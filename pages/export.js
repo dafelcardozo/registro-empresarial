00e4e27b5a9e205cc7e3a1b5b2abb969a7caa4b5
Felipe Cardozo <dafelcardozo@gmail.com>
2023-03-01 16:39:49 -0500

    Npm update.

--- pages/export.js
+++ pages/export.js
@@ -4,7 +4,7 @@ import autoTable from 'jspdf-autotable'
 
 
 export default function exportCompaniesToPDF() {
-    if (process.browser) {
+  if (process.browser) {
     const pdf = new jsPDF('l', 'pt', 'a0');
     autoTable(pdf, {
         html: '#listado_empresas',
@@ -17,10 +17,9 @@ export default function exportCompaniesToPDF() {
           1: {columnWidth: 'auto'}
         }
       });
-      
-      const iframe = document.createElement('iframe');
-      iframe.setAttribute('style', 'position:absolute;top:0;right:0;height:100%; width:600px');
-      document.body.appendChild(iframe);
-      iframe.src = pdf.output('datauristring');
-    }
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('style', 'position:absolute;top:0;right:0;height:100%; width:600px');
+    document.body.appendChild(iframe);
+    iframe.src = pdf.output('datauristring');
+  }
 }
